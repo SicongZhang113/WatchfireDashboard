cad8413078bc91ad399d2fb0cd805d0fc31e72ac
Sicong Zhang <sicongzhang@sicongdembp.lan>
2020-03-09 21:27:29 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+//import Header from './component/Header'
+//import Content from './component/Content'
+import Dashboard from './component/example/Dashboard.js'
+function App() {
+  return (
+    <div className="App">
+       
+       <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Content/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Content() {
+  return (
+    <div className="Content">
+       <p> content </p>
+    </div>
+  );
+}
+
+export default Content;
